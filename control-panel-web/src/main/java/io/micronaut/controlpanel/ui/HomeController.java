5018ecf49c2e5c4af02e8e5c86eefab453a2dc19
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2023-02-07 10:47:00 +0100

    Clearly show active environments

--- control-panel-web/src/main/java/io/micronaut/controlpanel/ui/HomeController.java
+++ control-panel-web/src/main/java/io/micronaut/controlpanel/ui/HomeController.java
@@ -4,13 +4,14 @@ import com.github.jknack.handlebars.Handlebars;
 import com.github.jknack.handlebars.HumanizeHelper;
 import com.github.jknack.handlebars.helper.ConditionalHelpers;
 import com.github.jknack.handlebars.helper.StringHelpers;
-import io.micronaut.context.annotation.Value;
+import io.micronaut.context.env.Environment;
 import io.micronaut.controlpanel.core.ControlPanel;
 import io.micronaut.controlpanel.core.ControlPanelRepository;
 import io.micronaut.core.version.VersionUtils;
 import io.micronaut.http.HttpResponse;
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
+import io.micronaut.runtime.ApplicationConfiguration;
 import io.micronaut.views.View;
 import java.util.Collection;
 import java.util.List;
@@ -28,12 +29,14 @@ public class HomeController {
     private final Handlebars handlebars;
     private final ControlPanelRepository repository;
 
-    private final String applicationName;
+    private final ApplicationConfiguration applicationConfiguration;
+    private final Environment environment;
 
-    public HomeController(Handlebars handlebars, ControlPanelRepository repository, @Value("${micronaut.application.name}") String applicationName) {
+    public HomeController(Handlebars handlebars, ControlPanelRepository repository, ApplicationConfiguration applicationConfiguration, Environment environment) {
         this.handlebars = handlebars;
         this.repository = repository;
-        this.applicationName = applicationName;
+        this.applicationConfiguration = applicationConfiguration;
+        this.environment = environment;
 
         //TODO move this to a better place
         HumanizeHelper.register(handlebars);
@@ -87,7 +90,8 @@ public class HomeController {
         List<ControlPanel> controlPanels = repository.findAllByCategory(categoryId);
         return HttpResponse.ok(Map.of(
             "micronautVersion", VersionUtils.getMicronautVersion(),
-            "applicationName", applicationName,
+            "applicationName", applicationConfiguration.getName().orElse("(unnamed)"),
+            "activeEnvironments", environment.getActiveNames(),
             "controlPanels", controlPanels,
             "categories", categories,
             "currentCategory", currentCategory,
@@ -104,7 +108,8 @@ public class HomeController {
         ControlPanel.Category currentCategory = repository.findCategoryById(controlPanel.getCategory().id());
         return HttpResponse.ok(Map.of(
             "micronautVersion", VersionUtils.getMicronautVersion(),
-            "applicationName", applicationName,
+            "applicationName", applicationConfiguration.getName().orElse("(unnamed)"),
+            "activeEnvironments", environment.getActiveNames(),
             "controlPanel", controlPanel,
             "categories", categories,
             "currentCategory", currentCategory,
