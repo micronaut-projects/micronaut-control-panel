91abcf899b44a0b9301b5c04ffc3e6959ee7a48a
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2025-10-02 11:04:20 +0200

    Do not enable agent in CI

--- buildSrc/src/main/kotlin/io.micronaut.build.internal.control-panel-example.gradle.kts
+++ buildSrc/src/main/kotlin/io.micronaut.build.internal.control-panel-example.gradle.kts
@@ -35,7 +35,7 @@ tasks.withType<Test> {
 
 graalvmNative {
     agent {
-        enabled.set(true)
+        enabled.set(System.getenv("CI") == null)
         defaultMode.set("conditional")
         modes {
             conditional {
