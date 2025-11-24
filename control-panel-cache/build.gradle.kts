114f09d012c23644b8330c3a4256c28e9d675503
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2025-11-24 19:12:49 +0100

    First cut of getBadge()

--- control-panel-cache/build.gradle.kts
+++ control-panel-cache/build.gradle.kts
@@ -13,5 +13,5 @@ dependencies {
 }
 
 micronautBuild {
-    binaryCompatibility.enabledAfter("1.10.0")
+    binaryCompatibility.enabledAfter("2.0.0")
 }
--- control-panel-cache/src/main/java/io/micronaut/controlpanel/panels/cache/CacheControlPanel.java
+++ control-panel-cache/src/main/java/io/micronaut/controlpanel/panels/cache/CacheControlPanel.java
@@ -1,14 +1,22 @@
 package io.micronaut.controlpanel.panels.cache;
 
+import io.micronaut.cache.AbstractMapBasedSyncCache;
 import io.micronaut.cache.Cache;
-import io.micronaut.cache.SyncCache;
+import io.micronaut.cache.caffeine.DefaultSyncCache;
+import io.micronaut.cache.ehcache.EhcacheSyncCache;
+import io.micronaut.cache.hazelcast.HazelcastAsyncCache;
+import io.micronaut.cache.hazelcast.HazelcastSyncCache;
+import io.micronaut.cache.infinispan.InfinispanAsyncCache;
+import io.micronaut.cache.infinispan.InfinispanSyncCache;
+import io.micronaut.cache.jcache.JCacheSyncCache;
 import io.micronaut.context.annotation.EachBean;
 import io.micronaut.context.annotation.Parameter;
 import io.micronaut.controlpanel.core.AbstractEachBeanControlPanel;
 import io.micronaut.controlpanel.core.config.ControlPanelConfiguration;
 import io.micronaut.core.annotation.ReflectiveAccess;
-import io.micronaut.core.async.publisher.Publishers;
 import jakarta.inject.Named;
+
+import java.util.stream.StreamSupport;
 
 /**
  * A control panel for managing caches.
@@ -17,17 +25,16 @@ import jakarta.inject.Named;
  * @since 1.10.0
  */
 @EachBean(Cache.class)
-public class CacheControlPanel extends AbstractEachBeanControlPanel<CacheControlPanel.Body> {
+public class CacheControlPanel<C> extends AbstractEachBeanControlPanel<CacheControlPanel.Body> {
 
     public static final String NAME = "cache";
     public static final String ENABLED_PROPERTY = ControlPanelConfiguration.PREFIX + "." + NAME + ".enabled";
 
-    private final Cache<?> cache;
+    private final Cache<C> cache;
 
-    public CacheControlPanel(@Parameter Cache<?> cache, @Named(NAME) ControlPanelConfiguration configuration) {
+    public CacheControlPanel(@Parameter Cache<C> cache, @Named(NAME) ControlPanelConfiguration configuration) {
         super(NAME, configuration);
         this.cache = cache;
-        SyncCache s;
     }
 
     @Override
@@ -47,7 +54,25 @@ public class CacheControlPanel extends AbstractEachBeanControlPanel<CacheControl
 
     @Override
     public String getBadge() {
-        return super.getBadge();
+        return switch (this.cache) {
+            case DefaultSyncCache caffeineCache ->
+                String.valueOf(caffeineCache.getNativeCache().estimatedSize());
+            case EhcacheSyncCache ehcache ->
+                String.valueOf(StreamSupport.stream(ehcache.getNativeCache().spliterator(), false).count());
+            case HazelcastSyncCache hazelcastCache ->
+                String.valueOf(hazelcastCache.getNativeCache().size());
+            case HazelcastAsyncCache hazelcastCache ->
+                String.valueOf(hazelcastCache.getNativeCache().size());
+            case InfinispanSyncCache infinispanCache ->
+                String.valueOf(infinispanCache.getNativeCache().size());
+            case InfinispanAsyncCache infinispanCache ->
+                String.valueOf(infinispanCache.getNativeCache().size());
+            case JCacheSyncCache jCache ->
+                String.valueOf(StreamSupport.stream(jCache.getNativeCache().spliterator(), false).count());
+            case AbstractMapBasedSyncCache mapCache ->
+                String.valueOf(mapCache.getNativeCache().size());
+            case null, default -> super.getBadge();
+        };
     }
 
     @Override
