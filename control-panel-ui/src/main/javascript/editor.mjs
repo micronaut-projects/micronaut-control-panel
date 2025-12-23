36d248f39af4ffd1794c1241de12db82fc8246b7
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2025-12-23 18:21:09 +0100

    Configure dialect

--- control-panel-ui/src/main/javascript/editor.mjs
+++ control-panel-ui/src/main/javascript/editor.mjs
@@ -1,30 +1,40 @@
 import {EditorView} from "@codemirror/view";
 import {EditorState} from "@codemirror/state";
 import {basicSetup} from "codemirror";
-import {sql, StandardSQL} from "@codemirror/lang-sql"
+import {sql, StandardSQL, PostgreSQL, MySQL, MariaSQL, MSSQL} from "@codemirror/lang-sql"
 
-const extensions = [
-    basicSetup,
-    sql({
-        dialect: StandardSQL,
-        upperCaseKeywords: true,
-    })
-]
-
-const state = EditorState.create({
-    doc: "/* Type an SQL query here, or select a table from the tree */\n\n",
-    extensions: extensions
-});
-
-const view = new EditorView({
-    state,
-    parent: document.querySelector("#sql-console")
-});
+function getSQLExtensions(databaseType) {
+    let dbDialect;
+    switch (databaseType.toUpperCase()) {
+        case 'MYSQL':
+            dbDialect = MySQL;
+            break;
+        case 'MARIADB':
+            dbDialect = MariaSQL;
+            break;
+        case 'POSTGRES':
+            dbDialect = PostgreSQL;
+            break;
+        case 'SQL_SERVER':
+            dbDialect = MSSQL;
+            break;
+        case 'ORACLE':
+        case 'GENERIC':
+        default:
+            dbDialect = StandardSQL;
+            break;
+    }
+    return [
+        basicSetup,
+        sql({
+            dialect: dbDialect,
+            upperCaseKeywords: true,
+        })
+    ];
+}
 
 window.codemirror = {
     EditorState,
     EditorView,
-    state,
-    view,
-    extensions
+    getSQLExtensions
 };
