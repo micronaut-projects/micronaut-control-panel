ab2d8fb1f2e4c6e65fb18aa7edcb607246b7e701
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-01-30 17:42:58 +0100

    Cleanup

--- doc-examples/example-java/src/main/java/example/kafka/KafkaTopicsFactory.java
+++ doc-examples/example-java/src/main/java/example/kafka/KafkaTopicsFactory.java
@@ -11,7 +11,7 @@ import org.apache.kafka.clients.admin.NewTopic;
 public class KafkaTopicsFactory {
 
     public static final String KAFKA_TOPIC_WORDS = "words";
-    public static final String KAFKA_TOPIC_COUNTS = "subTopologies";
+    public static final String KAFKA_TOPIC_COUNTS = "counts";
 
     @Bean
     NewTopic words() {
