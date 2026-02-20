20c913d37386b978ed0280b09a678a6ca6acdfa7
Sergio del Amo <sergio.delamo@softamo.com>
2026-02-20 17:52:01 +0100

    build: remove sonatype plugin config
    it is configured by Micronaut Build Gradle plugin

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -10,7 +10,6 @@ repositories {
 dependencies {
     implementation(libs.gradle.micronaut)
     implementation(libs.gradle.kotlin)
-    implementation(libs.sonatype.scan)
     implementation(libs.micronaut.shared.settings)
     implementation(libs.shadow.plugin)
     implementation(libs.test.logger.plugin)
--- buildSrc/src/main/kotlin/io.micronaut.build.internal.control-panel-module.gradle.kts
+++ buildSrc/src/main/kotlin/io.micronaut.build.internal.control-panel-module.gradle.kts
@@ -1,22 +1,6 @@
 plugins {
     id("io.micronaut.build.internal.module")
-    id("org.sonatype.gradle.plugins.scan")
 }
-
-val ossIndexUsername = System.getenv("OSS_INDEX_USERNAME") ?: project.properties["ossIndexUsername"] as String?
-val ossIndexPassword = System.getenv("OSS_INDEX_PASSWORD") ?: project.properties["ossIndexPassword"] as String?
-val sonatypePluginConfigured = ossIndexUsername != null && ossIndexPassword != null
-
-ossIndexAudit {
-    excludeCoordinates.addAll(listOf(
-        "com.google.guava:guava:18.0"
-    ))
-    if (sonatypePluginConfigured) {
-        username = ossIndexUsername
-        password = ossIndexPassword
-    }
-}
-
 repositories {
     mavenCentral()
 }
