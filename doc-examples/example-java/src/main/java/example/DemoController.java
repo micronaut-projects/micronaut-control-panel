296c454f59349e445de541e0742126930da080a5
Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-03-11 13:57:50 +0100

    Upgrade Micronaut OpenAPI to 7.0.0-M1 and restore example OpenAPI wiring (#491)

--- doc-examples/example-java/src/main/java/example/DemoController.java
+++ doc-examples/example-java/src/main/java/example/DemoController.java
@@ -2,20 +2,20 @@ package example;
 
 import io.micronaut.http.annotation.Controller;
 import io.micronaut.http.annotation.Get;
-//import io.swagger.v3.oas.annotations.Operation;
-//import io.swagger.v3.oas.annotations.tags.Tag;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 
-//@Tag(name = "demo")
+@Tag(name = "demo")
 @Controller("/demo")
 public class DemoController {
 
-//    @Operation(summary = "test1")
+    @Operation(summary = "test1")
     @Get("/test1")
     public String test1() {
         return "test1";
     }
 
-//    @Operation(summary = "test2")
+    @Operation(summary = "test2")
     @Get("/test2")
     public String test2() {
         return "test2";
