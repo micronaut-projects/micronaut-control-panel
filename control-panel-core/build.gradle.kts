fcdaa7a1fd55382ef5cf3cd8c96938108386359f
AI on behalf of Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-05-13 17:01:16 +0000

    Expose discovery dependency from core

--- control-panel-core/build.gradle.kts
+++ control-panel-core/build.gradle.kts
@@ -4,7 +4,7 @@ plugins {
 
 dependencies {
     api(mn.micronaut.router)
-    implementation(mn.micronaut.discovery.core)
+    api(mn.micronaut.discovery.core)
     implementation(mn.micronaut.http.server)
     implementation(mn.micronaut.json.core)
     compileOnly(libs.micronaut.security)
