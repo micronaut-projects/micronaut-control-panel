041f8e68156c8142eb528e49030fa5bf488a23bd
AI on behalf of Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-05-13 17:29:37 +0000

    Use server startup event for control panel URL logging

--- control-panel-core/build.gradle.kts
+++ control-panel-core/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     api(mn.micronaut.router)
-    api(mn.micronaut.discovery.core)
     implementation(mn.micronaut.http.server)
     implementation(mn.micronaut.json.core)
     compileOnly(libs.micronaut.security)
