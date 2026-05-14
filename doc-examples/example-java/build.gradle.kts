d432d2895110e74178e39db9054ff4a6bc89bb5b
AI on behalf of Álvaro Sánchez-Mariscal <alvaro.sanchez-mariscal@oracle.com>
2026-05-14 11:50:46 +0000

    Fix test resources control panel classpath

--- doc-examples/example-java/build.gradle.kts
+++ doc-examples/example-java/build.gradle.kts
@@ -67,6 +67,8 @@ dependencies {
     testImplementation(mnTest.micronaut.test.junit5)
     testImplementation(libs.playwright)
     testRuntimeOnly(mnTest.junit.platform.suite)
+    testResourcesRuntimeOnly(mn.micronaut.discovery.core)
+    testResourcesRuntimeOnly(mn.micronaut.management)
 }
 
 micronaut {
